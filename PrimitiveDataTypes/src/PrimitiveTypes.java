1260a59a12aa2ba8b8217caf9787871ba3ca0573
rapaich <rapaich@mail.ru>
2017-11-05 12:45:56 +0300

    create Primitive Data Types

---
+++ PrimitiveDataTypes/src/PrimitiveTypes.java
@@ -0,0 +1,13 @@
+public class PrimitiveTypes {
+    public static void main(String[] args) {
+
+        byte first = 57, second = -17;
+
+        System.out.println("The first variable contains value " + first);
+        System.out.println("The second variable contains value " + second);
+        System.out.println("Sum of variables contains value " + (first + second));
+        System.out.println("Difference of variables contains value " + (first - second));
+        System.out.println("Composition of variables contains value " + (first * second));
+        System.out.println("Private of variables contains value " + (first / second));
+    }
+}
