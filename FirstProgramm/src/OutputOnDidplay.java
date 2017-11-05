80ff3b295ca81c12b340cb35042beed7c3a45f8d
rapaich <rapaich@mail.ru>
2017-11-05 12:45:03 +0300

    create First Programm

---
+++ FirstProgramm/src/OutputOnDidplay.java
@@ -0,0 +1,16 @@
+public class OutputOnDidplay {
+    public static void main(String[] args) {
+        System.out.print("I made fist step in Java \r\n");
+        System.out.print("I made fist step in Java \r\n");
+        System.out.print("I made fist step in Java \r\n");
+        System.out.print("I made fist step in Java \r\n");
+        System.out.print("I made fist step in Java \r\n");
+
+        System.out.print("I made fist step in Java  ");
+        System.out.print("I made fist step in Java  ");
+        System.out.print("I made fist step in Java  ");
+        System.out.print("I made fist step in Java  ");
+        System.out.print("I made fist step in Java  ");
+
+    }
+}
