2b835ab9caa4d43fd58c67dec328b1c7eb22ef9e
rapaich <rapaich@mail.ru>
2017-11-20 02:15:25 +0300

    create Lines

---
+++ Lines/src/Lines.java
@@ -0,0 +1,14 @@
+public class Lines {
+
+    public static void main(String[] args) {
+
+        String str = "Теперь я умею создавать строки.";
+        byte bit = 70;
+
+        str = str + " Я даже прикрепил переменную к этой строке. Её значение: " + bit;
+
+        System.out.println(str);
+
+    }
+
+}
