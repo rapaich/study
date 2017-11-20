22123a9a198711a46557c48338e9e2747a5c965c
rapaich <rapaich@mail.ru>
2017-11-20 19:03:30 +0300

    create String Comparison

---
+++ StringComparison/src/StringComparison.java
@@ -0,0 +1,14 @@
+public class StringComparison {
+
+    public static void main(String[] args) {
+
+        String foot = new String("Нога");
+        String foot1 = new String("Нога");
+
+        boolean abc = foot.equals(foot1);
+
+        System.out.println(!abc);
+
+    }
+
+}
